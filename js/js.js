c50e94ce328e625d7082eed12bca992902740186
Daryna Toporovska <Daryna.Toporovska@student.souepl.cz>
2024-09-06 11:30:24 +0200

    javakurva

---
+++ js/js.js
@@ -0,0 +1,27 @@
+function Add(){
+    let input = document.getElementById("input"); 
+    document.getElementById("output").innerHTML = input.value;
+}
+
+
+
+
+// v githubu to bude
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
